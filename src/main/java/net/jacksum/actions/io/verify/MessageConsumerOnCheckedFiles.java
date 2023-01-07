fccc5b77cccc44633f6cbc771d8a12f36131b1a4
Johann N. Löfflmann <johann@loefflmann.net>
2023-01-07 22:34:46 +0100

    3.5.0 -> 3.6.0

--- src/main/java/net/jacksum/actions/io/verify/MessageConsumerOnCheckedFiles.java
+++ src/main/java/net/jacksum/actions/io/verify/MessageConsumerOnCheckedFiles.java
@@ -1,6 +1,6 @@
 /*
 
-  Jacksum 3.5.0 - a checksum utility in Java
+  Jacksum 3.6.0 - a checksum utility in Java
   Copyright (c) 2001-2023 Dipl.-Inf. (FH) Johann N. Löfflmann,
   All Rights Reserved, <https://jacksum.net>.
 
