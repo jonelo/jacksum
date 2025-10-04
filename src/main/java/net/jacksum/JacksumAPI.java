f0d94961db974573f6067c66f5fd0a448a77b2d0
Johann N. Loefflmann <johann@loefflmann.net>
2025-10-04 16:07:03 +0200

    updated Copyright year

--- src/main/java/net/jacksum/JacksumAPI.java
+++ src/main/java/net/jacksum/JacksumAPI.java
@@ -58,7 +58,7 @@ public class JacksumAPI {
     /**
      * The Copyright of the program
      */
-    public final static String COPYRIGHT = "Copyright (C) 2001-2006, 2021-2024, Dipl.-Inf. (FH) Johann N. Loefflmann";
+    public final static String COPYRIGHT = "Copyright (C) 2001-2006, 2021-2025, Dipl.-Inf. (FH) Johann N. Loefflmann";
 
     /**
      * Returns a Version object of this API.
@@ -120,6 +120,10 @@ public class JacksumAPI {
      */
     public static Map<String, String> getAvailableAlgorithms() {
         return HashFunctionFactory.getAvailableAlgorithms();
+    }
+
+    public static Map<String, String> getAvailableHMACs() {
+        return HashFunctionFactory.getAvailableHMACs();
     }
 
     /**
